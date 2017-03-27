50dbe15c5ce8f7afd926f504173d75e6fcb9abec
every-sushi <nadia0515@naver.com>
2017-03-27 19:08:11 +0900

    #2 Java Event complete

--- app/src/main/java/rlaskfud0515/kr/hs/emirim/edittexttest1/MainActivity.java
+++ app/src/main/java/rlaskfud0515/kr/hs/emirim/edittexttest1/MainActivity.java
@@ -2,12 +2,29 @@ package rlaskfud0515.kr.hs.emirim.edittexttest1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edit1;
+    TextView textResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edit1 = (EditText)findViewById(R.id.edit1);
+        Button butOk = (Button) findViewById(R.id.but_ok);
+        textResult= (TextView) findViewById(R.id.text_result);
+
+        butOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = edit1.getText().toString();
+                textResult.setText("인재요인재요");
+            }
+        });
     }
 }
